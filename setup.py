c3822b73883f56d9ca38b49c4dabf8ef7fee006b
David Cannings <david@edeca.net>
2025-01-07 09:58:43 +0000

    Bump dependencies to fix Jinja2 security issue

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
         "Topic :: Security",
     ],
-    install_requires=["Jinja2==3.1.3",],
+    install_requires=["Jinja2==3.1.5",],
     extras_require={
         "docs": docs_require,
         "tests": tests_require,
